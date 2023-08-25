009d94ee0c5ceabf631eaac884be7c18561ada3f
Eyad-al-moubaed <eyadmax1980@gmail.com>
2023-08-25 15:42:18 +0300

    deployed website

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/Store';
+import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    
+<HashRouter>
     <Provider store={store}><App /></Provider> 
+</HashRouter>   
 
 
 );
